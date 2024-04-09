89664738f29ce47640ae14303cc6e2e2a5e22f4f
Frankenmolen,Joep Frankenmolen J <joep.frankenmolen@gmail.com>
2024-04-09 15:44:24 +0200

    t

---
+++ turtlebot3_teleop/turtlebot3_teleop/script/Odometry.py
@@ -0,0 +1,25 @@
+import math
+
+class Quaternion:
+    w: float
+    x: float
+    y: float
+    z: float
+
+def quaternion_from_euler(roll, pitch, yaw):
+    """
+    Converts euler roll, pitch, yaw to quaternion
+    """
+    cy = math.cos(yaw * 0.5)
+    sy = math.sin(yaw * 0.5)
+    cp = math.cos(pitch * 0.5)
+    sp = math.sin(pitch * 0.5)
+    cr = math.cos(roll * 0.5)
+    sr = math.sin(roll * 0.5)
+
+    q = Quaternion()
+    q.w = cy * cp * cr + sy * sp * sr
+    q.x = cy * cp * sr - sy * sp * cr
+    q.y = sy * cp * sr + cy * sp * cr
+    q.z = sy * cp * cr - cy * sp * sr
+    return q 
